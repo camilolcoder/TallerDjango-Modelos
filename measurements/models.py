e526fb92f1c4cec2db35adbdd2c4495b06a66652
camilolcoder <j.colmenares@uniandes.edu.co>
2023-02-05 12:59:35 -0500

    taller listo

--- measurements/models.py
+++ measurements/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 
-# Create your models here.
+from variables.models import Variable
+#from alarms.models import Alarms
+
+class Measurement(models.Model):
+    variable = models.ForeignKey(Variable, on_delete=models.CASCADE, default=None)
+    #alarm = models.ManyToManyField(Alarms, default=None)
+    value = models.FloatField(null=True, blank=True, default=None)
+    unit = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.value, self.unit)
